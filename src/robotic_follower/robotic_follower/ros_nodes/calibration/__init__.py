ac633987292ab8416aaf9b6f8692b62087599223
srsnn <srsnng@hotmail.com>
2026-04-17 14:50:56 +0800

    fix 移除已删除模块的注册

--- src/robotic_follower/robotic_follower/ros_nodes/calibration/__init__.py
+++ src/robotic_follower/robotic_follower/ros_nodes/calibration/__init__.py
@@ -1,14 +1,8 @@
-"""标定模块 ROS 节点。"""
+"""标定模块 ROS 节点"""
 
-from .calculator_node import CalibrationCalculatorNode
 from .chessboard_tf_node import ChessboardTFNode
-from .result_manager_node import CalibrationResultManagerNode
-from .tf_publisher_node import TFPublisherNode
 
 
 __all__ = [
-    "CalibrationCalculatorNode",
-    "CalibrationResultManagerNode",
-    "TFPublisherNode",
     "ChessboardTFNode",
 ]
